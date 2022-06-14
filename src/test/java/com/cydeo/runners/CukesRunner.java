37b23636598bb6d185814d641fbd0d239cc7a2eb
Mehmet YILDIRIM <97946779+inmyildirim@users.noreply.github.com>
2022-06-14 12:14:56 +0300

    Initial commit

---
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -0,0 +1,17 @@
+package com.cydeo.runners;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/cydeo/step_definitions",
+        dryRun = false
+)
+public class CukesRunner {
+
+}
