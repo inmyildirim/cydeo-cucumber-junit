b42a074017dd1a9845b80948d4c52208abf498db
Mehmet YILDIRIM <97946779+inmyildirim@users.noreply.github.com>
2022-06-15 02:09:08 +0300

    day15 login.feature

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -1,7 +1,5 @@
 package com.cydeo.runners;
 
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
